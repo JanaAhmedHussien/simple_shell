2d1d82a95b8d45ad41b5a3d35c25b962d31151e9
jana <jana.ahmedhussein244@gmail.com>
2023-09-19 15:03:38 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - our main shell function
+ * @ac: numbering parameters
+ * @av: argument vector
+ * Return: 0(success).
+ */
+int main(int ac, char **av)
+{
+char **cmd = NULL;
+char *input = NULL;
+int checking = 0;
+void (ac);
+void (av);
+
+while (true)
+{
+input = read_input();
+if (input == NULL)
+{
+if (isatty(STDIN_FILENO) == 1)
+{
+write(STDOUT_FILENO, "\n", 1);
+}
+return (checking);
+}
+printf("%s", input);
+free(input);
+}
+
+
+
+}
